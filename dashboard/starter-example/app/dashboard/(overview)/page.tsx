b12af5f569578336f64b634d60bec603afbbebcd
nxchien-th <nguyenxuanchien92@gmail.com>
2025-04-20 09:27:18 +0700

    chaper 11---adding search and pagination

--- dashboard/starter-example/app/dashboard/(overview)/page.tsx
+++ dashboard/starter-example/app/dashboard/(overview)/page.tsx
@@ -1,8 +1,8 @@
-import RevenueChart from '../../ui/dashboard/revenue-chart';
-import LatestInvoices from '../../ui/dashboard/latest-invoices';
-import { lusitana } from '../../ui/fonts';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app//ui/fonts';
 import { Suspense } from 'react';
-import CardWrapper from '../../ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
